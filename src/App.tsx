564722dc3c8ea581f4c0b762640bc96f307a7cc5
Hanzala <bigapplestorekmr@gmail.com>
2025-06-15 18:40:31 +0530

    over

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 
-import atriumLogo from "./assets/atrium.jpeg";
+import atriumLogo from "./assets/atrium.png";
 import atrium2 from "./assets/atrium2.png";
 import leftImage from "./assets/atrium3-.png";
 import rightImage from "./assets/atrium4.png";
 import dex from "./assets/dex.jpeg";
-import aaron from "./assets/aaron.jpeg"
+import aaron from "./assets/aaron.jpeg";
+import me from "./assets/hanzala.jpg"
 
 function App() {
   return (
@@ -25,7 +26,8 @@ function App() {
 
       <div className="flex justify-center md:space-x-20 items-center mt-30 ">
         <img src={atrium2} alt="lol" className="w-40 h-40"></img>
-        <div className="md:text-8xl text-3xl text-[#85ff99] hover:text-green-400">
+        <div className="md:text-8xl text-3xl text-[background: #2a9b86;
+background: linear-gradient(90deg, rgba(42, 155, 134, 1) 0%, rgba(87, 199, 133, 1) 50%, rgba(86, 228, 221, 1) 97%, rgba(114, 237, 83, 1) 100%);] hover:text-green-400">
           Where Developers <br></br>Ship Their Best Work
         </div>
       </div>
@@ -118,10 +120,35 @@ function App() {
       <div className="flex items-center mt-10 mx-10 md:mx-50 ">
         <div className="flex md:flex-row flex-col  space-y-14 md:space-x-90 items-center">
           <div>
-          <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#85ff99" stroke-width="1.5"><path fill-rule="evenodd" clip-rule="evenodd" d="M9.21255 12.75C9.12943 13.5242 8.9054 14.1421 8.5147 14.6891C7.99181 15.4211 7.11571 16.1036 5.66459 16.8292C5.29411 17.0144 5.14394 17.4649 5.32918 17.8354C5.51442 18.2059 5.96493 18.3561 6.33541 18.1708C7.88429 17.3964 9.00819 16.5789 9.7353 15.5609C10.4761 14.5238 10.75 13.3571 10.75 12V7.5C10.75 6.53351 9.96649 5.75 9 5.75H5C4.03351 5.75 3.25 6.53351 3.25 7.5V11C3.25 11.9665 4.03352 12.75 5 12.75H9.21255Z" fill="#85ff99"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M19.2125 12.75C19.1294 13.5242 18.9054 14.1421 18.5147 14.6891C17.9918 15.4211 17.1157 16.1036 15.6646 16.8292C15.2941 17.0144 15.1439 17.4649 15.3292 17.8354C15.5144 18.2059 15.9649 18.3561 16.3354 18.1708C17.8843 17.3964 19.0082 16.5789 19.7353 15.5609C20.4761 14.5238 20.75 13.3571 20.75 12V7.5C20.75 6.53352 19.9665 5.75 19 5.75H15C14.0335 5.75 13.25 6.53352 13.25 7.5V11C13.25 11.9665 14.0335 12.75 15 12.75H19.2125Z" fill="#85ff99"></path></svg>
+            <svg
+              width="30px"
+              height="30px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              color="#85ff99"
+              stroke-width="1.5"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M9.21255 12.75C9.12943 13.5242 8.9054 14.1421 8.5147 14.6891C7.99181 15.4211 7.11571 16.1036 5.66459 16.8292C5.29411 17.0144 5.14394 17.4649 5.32918 17.8354C5.51442 18.2059 5.96493 18.3561 6.33541 18.1708C7.88429 17.3964 9.00819 16.5789 9.7353 15.5609C10.4761 14.5238 10.75 13.3571 10.75 12V7.5C10.75 6.53351 9.96649 5.75 9 5.75H5C4.03351 5.75 3.25 6.53351 3.25 7.5V11C3.25 11.9665 4.03352 12.75 5 12.75H9.21255Z"
+                fill="#85ff99"
+              ></path>
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M19.2125 12.75C19.1294 13.5242 18.9054 14.1421 18.5147 14.6891C17.9918 15.4211 17.1157 16.1036 15.6646 16.8292C15.2941 17.0144 15.1439 17.4649 15.3292 17.8354C15.5144 18.2059 15.9649 18.3561 16.3354 18.1708C17.8843 17.3964 19.0082 16.5789 19.7353 15.5609C20.4761 14.5238 20.75 13.3571 20.75 12V7.5C20.75 6.53352 19.9665 5.75 19 5.75H15C14.0335 5.75 13.25 6.53352 13.25 7.5V11C13.25 11.9665 14.0335 12.75 15 12.75H19.2125Z"
+                fill="#85ff99"
+              ></path>
+            </svg>
 
-            <h1 className="text-xl md:h-20 h-70 mb-3 mt-5 w-60 md:w-100">UHI was an excellent experience. The depth of content, quality of instructors, and level of challenge made it incredibly valuable. It’s a program that truly stands out.</h1>
-            <div className="flex space-x-5 mt-12 md:mt-30">
+            <h1 className="text-xl md:h-20 h-70 mb-2 mt-5 w-70 md:w-100">
+              UHI was an excellent experience. The depth of content, quality of
+              instructors, and level of challenge made it incredibly valuable.
+              It’s a program that truly stands out.
+            </h1>
+            <div className="flex space-x-5 md:mt-30">
               <img src={dex} alt="dex" className="h-10 w-10" />
               <h2 className="flex flex-col space-y-1">
                 <h2> Dex Chen </h2>
@@ -130,11 +157,36 @@ function App() {
             </div>
           </div>
           <div className="md:mb-10">
-          <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#85ff99" stroke-width="1.5"><path fill-rule="evenodd" clip-rule="evenodd" d="M9.21255 12.75C9.12943 13.5242 8.9054 14.1421 8.5147 14.6891C7.99181 15.4211 7.11571 16.1036 5.66459 16.8292C5.29411 17.0144 5.14394 17.4649 5.32918 17.8354C5.51442 18.2059 5.96493 18.3561 6.33541 18.1708C7.88429 17.3964 9.00819 16.5789 9.7353 15.5609C10.4761 14.5238 10.75 13.3571 10.75 12V7.5C10.75 6.53351 9.96649 5.75 9 5.75H5C4.03351 5.75 3.25 6.53351 3.25 7.5V11C3.25 11.9665 4.03352 12.75 5 12.75H9.21255Z" fill="#85ff99"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M19.2125 12.75C19.1294 13.5242 18.9054 14.1421 18.5147 14.6891C17.9918 15.4211 17.1157 16.1036 15.6646 16.8292C15.2941 17.0144 15.1439 17.4649 15.3292 17.8354C15.5144 18.2059 15.9649 18.3561 16.3354 18.1708C17.8843 17.3964 19.0082 16.5789 19.7353 15.5609C20.4761 14.5238 20.75 13.3571 20.75 12V7.5C20.75 6.53352 19.9665 5.75 19 5.75H15C14.0335 5.75 13.25 6.53352 13.25 7.5V11C13.25 11.9665 14.0335 12.75 15 12.75H19.2125Z" fill="#85ff99"></path></svg>
+            <svg
+              width="30px"
+              height="30px"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              color="#85ff99"
+              stroke-width="1.5"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M9.21255 12.75C9.12943 13.5242 8.9054 14.1421 8.5147 14.6891C7.99181 15.4211 7.11571 16.1036 5.66459 16.8292C5.29411 17.0144 5.14394 17.4649 5.32918 17.8354C5.51442 18.2059 5.96493 18.3561 6.33541 18.1708C7.88429 17.3964 9.00819 16.5789 9.7353 15.5609C10.4761 14.5238 10.75 13.3571 10.75 12V7.5C10.75 6.53351 9.96649 5.75 9 5.75H5C4.03351 5.75 3.25 6.53351 3.25 7.5V11C3.25 11.9665 4.03352 12.75 5 12.75H9.21255Z"
+                fill="#85ff99"
+              ></path>
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M19.2125 12.75C19.1294 13.5242 18.9054 14.1421 18.5147 14.6891C17.9918 15.4211 17.1157 16.1036 15.6646 16.8292C15.2941 17.0144 15.1439 17.4649 15.3292 17.8354C15.5144 18.2059 15.9649 18.3561 16.3354 18.1708C17.8843 17.3964 19.0082 16.5789 19.7353 15.5609C20.4761 14.5238 20.75 13.3571 20.75 12V7.5C20.75 6.53352 19.9665 5.75 19 5.75H15C14.0335 5.75 13.25 6.53352 13.25 7.5V11C13.25 11.9665 14.0335 12.75 15 12.75H19.2125Z"
+                fill="#85ff99"
+              ></path>
+            </svg>
 
-            <h1 className="text-xl md:h-20 h-70 mb-3 mt-5 w-60 md:w-100">Atrium has proven itself as a world-class partner, delivering unparalleled developer education and community-building. The results speak for themselves.</h1>
-            <div className="flex space-x-5 mt-10 md:mt-30">
-              <img src={aaron} alt="aaron" className="h-10 w-10"/>
+            <h1 className="text-xl md:h-20 h-70 md:mb-3 mt-5 w-70 md:w-100">
+              Atrium has proven itself as a world-class partner, delivering
+              unparalleled developer education and community-building. The
+              results speak for themselves.
+            </h1>
+            <div className="flex space-x-5 md:mt-30">
+              <img src={aaron} alt="aaron" className="h-10 w-10" />
               <h2 className="flex flex-col space-y-1">
                 <h2> Aaron Lamphere </h2>
                 <h3> Uniswap Foundation</h3>
@@ -143,6 +195,35 @@ function App() {
           </div>
         </div>
       </div>
+      <div className="flex md:flex-row justify-center md:ml-0 ml-10 mb-10 space-y-8  md:space-x-120 flex-col mt-10">
+        <div>
+          <ul className="text-2xl text-[#85ff99] mb-2"> Explore</ul>
+          <ul>about us</ul>
+          <ul>partner w us </ul>
+          <ul>uniswap hook incubator</ul>
+          <ul> uniswap hook course</ul>
+          <ul> hook directory</ul>
+          <ul> brand guidelines </ul>
+        </div>
+        <div>
+          <ul className="text-2xl text-[#85ff99] mb-2"> Community </ul>
+          <ul> x (twitter)</ul>
+          <ul> warpcast </ul>
+          <ul> linkedin </ul>
+          <ul>weekly-events</ul>
+          <ul>blogs</ul>
+        </div>
+        <div>
+          <ul className="text-2xl text-[#85ff99] mb-2"> Legal</ul>
+          <ul>terms of service </ul>
+          <ul>privacy policy </ul>
+        </div>
+      </div>
+      <div className="flex md:justify-center justify-start  ml-8 items-center space-x-2 "> 
+        <img src={me} className='h-9 w-9 rounded-4xl' alt="hanzala"></img>
+        <h1 className="hover:text-[#85ff99] text-lg mt-1 text-gray-500 "> hanzala 2025 </h1>
+      </div>
+      <br></br>
     </div>
   );
 }
